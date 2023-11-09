14f84624abdb646783ae9a48a8fb87aa9d7b1537
mauriciossn <mauricio.nasc@hotmail.com>
2023-11-09 10:33:52 -0300

    update

--- ArmandoLeite.Desktop.UI/Program.cs
+++ ArmandoLeite.Desktop.UI/Program.cs
@@ -11,7 +11,7 @@ namespace ArmandoLeite.Desktop.UI
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new AdicionarConteudo());
+            Application.Run(new VisualizarConteudo());
        }
     }
 }
