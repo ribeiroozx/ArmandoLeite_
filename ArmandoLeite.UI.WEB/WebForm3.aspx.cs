e55950e1d32e13960484346c53adf2cc3b3021de
MarcelVic <Marcelvictor75@gmail.com>
2023-11-27 11:11:34 -0300

    update

--- ArmandoLeite.UI.WEB/WebForm3.aspx.cs
+++ ArmandoLeite.UI.WEB/WebForm3.aspx.cs
@@ -8,26 +8,27 @@ using System.Data.SqlClient;
 
 namespace ArmandoLeite.UI.WEB
 {
-    public partial class WebForm1 : System.Web.UI.Page
+    
+
+    public partial class WebForm3 : System.Web.UI.Page
     {
+        public string id { get; set; }
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
 
-
-
         protected void BtnEnviar_Click1(object sender, EventArgs e)
         {
+            id = TextId.Text;
             SqlConnection conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TarefasBD;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             conn.Open();
 
-            SqlCommand cmd = new SqlCommand("insert into Cadastro values('" + TextNome.Text + "', '" + Convert.ToInt32(TextIdade.Text) + "', '" + TextEndereco.Text + "')", conn);
-            cmd.ExecuteNonQuery();
-
+            SqlCommand cmd = new SqlCommand("select * from Cadastro where idCadastro like '%" + id + "%'", conn);
+            SqlDataReader dr = cmd.ExecuteReader();
             System.Threading.Thread.Sleep(3000);
-            Response.Redirect("WebForm2.aspx?id=10");
-
+            Response.Redirect("WebForm2.aspx?"+id+"");
         }
     }
 }
